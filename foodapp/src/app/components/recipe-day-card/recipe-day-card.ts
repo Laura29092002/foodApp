d44376ed2a7ad3506acc575481c53870dade2cc3
laura2909 <mussardlaura299@gmail.com>
2026-01-13 11:17:45 +0100

    change design to create-planning page

--- foodapp/src/app/components/recipe-day-card/recipe-day-card.ts
+++ foodapp/src/app/components/recipe-day-card/recipe-day-card.ts
@@ -1,6 +1,8 @@
 import { Component, Input} from '@angular/core';
 import { Recipe } from '../../models/recipe/recipe.model';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import * as PlanningActions from '../../store/planning-store/planning-store.actions';
 @Component({
   selector: 'app-recipe-day-card',
   imports: [],
@@ -9,10 +11,30 @@ import { Router } from '@angular/router';
 })
 export class RecipeDayCard {
   @Input() recipe!: Recipe;
+  @Input() dayId!: number;
+  @Input() typeOfMeal!: string;
 
-  constructor(private router : Router) { }
+  constructor(private router : Router, private store: Store) { }
 
   onClick() {
     this.router.navigate(['/recipe', this.recipe.id]);
+  }
+
+  modifyRecipe(dayId: number, mealType: string) {
+    if(mealType == 'lunch' || mealType == 'dinner'){
+      this.store.dispatch(PlanningActions.startRecipeSelection({ 
+        dayId, 
+        mealType 
+      }));
+    }
+      this.router.navigate(["/recipes-select"]);
+    }
+  
+  removeRecipe(dayId: number, mealType: string) {
+    if(mealType == 'lunch' || mealType == 'dinner'){
+      if (confirm('Êtes-vous sûr de vouloir supprimer cette recette ?')) {
+        this.store.dispatch(PlanningActions.removeRecipeFromDay({ dayId, mealType }));
+      }
+    }
   }
 }
