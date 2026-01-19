0dfa4686297ad88b0816849077d72471e3f01790
laura2909 <mussardlaura299@gmail.com>
2026-01-19 14:32:16 +0100

    delete issues

--- foodapp/src/main.ts
+++ foodapp/src/main.ts
@@ -1,17 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-//bootstrapApplication(App, appConfig)
-//  .catch((err) => console.error(err));
-
-setTimeout(() => {
-
-  bootstrapApplication(App,{
-    providers: [
-      provideAnimations()
-    ]
-  });
-
-}, 3000);
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
